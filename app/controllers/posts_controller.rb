a44334a947065f288f861df3f382ce7ff605d753
Rikiya3702 <rikiya3702@gmail.com>
2020-03-03 22:28:02 +0900

    post controller最低限のCRUD追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,46 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    Post.find(params[:id]).destroy
+    redirect_to posts_path
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit( :title, :content, :public)
+    end
 end
