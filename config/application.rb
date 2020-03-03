ea9b5ad4ee56cbb5f990d87ca6f459306d114716
Rikiya3702 <rikiya3702@gmail.com>
2020-03-03 22:49:51 +0900

    RSpecの設定

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,13 @@ module Myapp
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rpec,
+      fixtures: true,
+      view_spects: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
