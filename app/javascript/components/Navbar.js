522f0758a8616ddc9656f4387c21a36b7f84dd13
Rikiya3702 <rikiya3702@gmail.com>
2020-03-03 17:13:05 +0900

    Header, Navbarを仮追加

---
+++ app/javascript/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Navbar extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <nav>
+          navでしてー
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Navbar
