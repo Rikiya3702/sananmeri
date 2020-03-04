de93d8c4da8914ad1026adb180f5573e4ee50956
Rikiya3702 <rikiya3702@gmail.com>
2020-03-04 17:20:52 +0900

    Deviseのi18n対応

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'html2haml'
 gem 'font-awesome-sass'
 gem 'rails-i18n'
 gem 'devise'
+gem 'devise-i18n'
+gem 'devise-i18n-views'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'posts#index'
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
-  
-  root 'posts#index'
 
   get 'home/index'
   get 'home/reacttutorial'
+
   resources :posts
 end
